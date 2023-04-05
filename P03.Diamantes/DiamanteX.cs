8b3c0aa78ba4bda2b52c0b532b3e104808b67f0d
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-04-05 14:21:30 -0300

    Refatoração utilizando Orientação a Objetos.

---
+++ P03.Diamantes/DiamanteX.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03.Diamantes
+{
+     internal class DiamanteX
+     {
+          public int numero;
+          public string opcao;
+
+          public string Opcao
+          {
+               set
+               {
+                    if (opcao == "S")
+                    {
+                         Console.ForegroundColor = ConsoleColor.Red;
+                         Console.WriteLine("\nSaindo...");
+                         Console.ResetColor();
+                         Environment.Exit(0);
+                    }
+               }
+          }
+
+          public string GerarMenu()
+          {
+               Console.Clear();
+               Console.WriteLine("===============================");
+               Console.WriteLine("\nDiamantes em X\n");
+               Console.WriteLine("===============================");
+
+               Console.Write("\nDigite S para sair, ou qualquer outro botão para continuar:\n> ");
+               string opcao;
+               opcao = Console.ReadLine().ToUpper();
+               return opcao;
+          }
+
+          public int PegarNumero(string mensagem)
+          {
+               Console.Clear();
+               Console.Write(mensagem);
+               numero = Convert.ToInt32(Console.ReadLine());
+               Console.ResetColor();
+
+               while (numero % 2 == 0)
+               {
+                    Console.Clear();
+                    Console.Write("Digite um número válido!\n\n");
+                    Console.Write(mensagem);
+                    numero = Convert.ToInt32(Console.ReadLine());
+               }
+               Console.Write("\n\n");
+               return numero;
+          }
+
+          public void ConstruirDiamante()
+          {
+               Console.ForegroundColor = ConsoleColor.Blue;
+               for (int i = 0; i < numero; i++)
+               {
+                    int espaco = Math.Abs(numero / 2 - i);
+                    int x = numero - 2 * espaco;
+
+                    for (int p = 0; p < espaco; p++)
+                    {
+                         Console.Write(" ");
+                    }
+                    for (int p = 0; p < x; p++)
+                    {
+                         Console.Write("X");
+                    }
+                    Console.WriteLine();
+               }
+               Console.ReadLine();
+               Console.ResetColor();
+          }
+
+     }
+}
--- P03.Diamantes/Program.cs
+++ P03.Diamantes/Program.cs
@@ -1,88 +1,19 @@
 ﻿namespace P03.Diamantes
 {
      internal class Program
-     {
-          #region Variável Global;
-
-          static int numero;
-
-          #endregion
-
+     {                
           static void Main(string[] args)
           {
+               DiamanteX diamante1 = new DiamanteX();
+
                do
                {
-                    string opcao = GerarMenu();
-
-                    if (opcao == "S")
-                    {
-                         Console.ForegroundColor = ConsoleColor.Red;
-                         Console.WriteLine("\nSaindo...");
-                         Console.ResetColor();
-                         break;
-                    }
-
-                    PegarNumero("Entre com um valor ímpar:\n> ");
-                    Console.Write("\n\n");
-                    ConstruirDiamante();
+                    diamante1.opcao = diamante1.GerarMenu();
+                    diamante1.Opcao = diamante1.opcao;
+                    diamante1.numero = diamante1.PegarNumero("Entre com um valor ímpar:\n> ");                 
+                    diamante1.ConstruirDiamante();
 
                } while (true);
           }
-
-          #region Funções;
-
-          static string GerarMenu()
-          {
-               Console.Clear();
-               Console.WriteLine("===============================");
-               Console.WriteLine("\nDiamantes em X\n");
-               Console.WriteLine("===============================");
-
-               Console.Write("\nDigite S para sair, ou qualquer outro botão para continuar:\n> ");
-               string opcao;
-               opcao = Console.ReadLine().ToUpper();
-               return opcao;
-          }
-
-          static void PegarNumero(string mensagem)
-          {
-               Console.Clear();
-               Console.Write(mensagem);
-               numero = Convert.ToInt32(Console.ReadLine());
-               Console.ResetColor();
-
-               while (numero % 2 == 0)
-               {
-                    Console.Clear();
-                    Console.Write("Digite um número válido!\n\n");
-                    Console.Write(mensagem);
-                    numero = Convert.ToInt32(Console.ReadLine());
-               }
-          }
-
-          static void ConstruirDiamante()
-          {
-               Console.ForegroundColor = ConsoleColor.Blue;
-               for (int i = 0; i < numero; i++)
-               {
-                    int espaco = Math.Abs(numero / 2 - i);
-                    int x = numero - 2 * espaco;
-
-                    for (int p = 0; p < espaco; p++)
-                    {
-                         Console.Write(" ");
-                    }
-                    for (int p = 0; p < x; p++)
-                    {
-                         Console.Write("X");
-                    }
-                    Console.WriteLine();
-               }
-               Console.ReadLine();
-               Console.ResetColor();
-          }
-
-          #endregion
-
      }
 }
